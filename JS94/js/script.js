6741575c62c8c2f55790fd94f53c27b63f7bb2fc
Valery <11w-walerik@mail.ru>
2022-10-14 16:35:55 +0300

    96 done

--- JS94/js/script.js
+++ JS94/js/script.js
@@ -421,11 +421,15 @@ window.addEventListener('DOMContentLoaded', () => {
         dots.push(dot);
     }
 
+    function strToNumber(str) {
+        return +str.replace(/\D/g, '');
+    }
+
     next.addEventListener('click', () => {
-        if (offset == +width.slice(0, width.length - 2) * (slides.length - 1)) {
+        if (offset == strToNumber(width) * (slides.length - 1)) {
             offset = 0;
         } else {
-            offset += +width.slice(0, width.length - 2);
+            offset += strToNumber(width);
         }
 
         slidesField.style.transform = `translateX(-${offset}px)`;   // сдвигаем слайды на отступ
@@ -448,9 +452,9 @@ window.addEventListener('DOMContentLoaded', () => {
 
     prev.addEventListener('click', () => {
         if (offset == 0) {
-            offset = +width.slice(0, width.length - 2) * (slides.length - 1);
+            offset = strToNumber(width) * (slides.length - 1);
         } else {
-            offset -= +width.slice(0, width.length - 2);
+            offset -= strToNumber(width);
         }
 
         slidesField.style.transform = `translateX(-${offset}px)`;   // сдвигаем слайды на отступ
@@ -476,7 +480,7 @@ window.addEventListener('DOMContentLoaded', () => {
             const slideTo = e.target.getAttribute('data-slide-to');
 
             slideIndex = slideTo;
-            offset = +width.slice(0, width.length - 2) * (slideTo - 1);
+            offset = strToNumber(width) * (slideTo - 1);
 
             slidesField.style.transform = `translateX(-${offset}px)`;   // сдвигаем слайды на отступ
 
---
+++ JS96/script.js
@@ -0,0 +1,64 @@
+'use strict';
+
+// классический синтаксис
+// new RegExp('pattern', 'flags');
+
+// синтаксис короче и проще
+// /pattern/f
+
+// search
+const ans = prompt('Введите ваше число');
+//const reg = /n/ig;
+const reg = /\d/g;
+
+// test - метод ищет в строке паттерн регулярного выражения
+//console.log(reg.test(ans));
+
+//console.log(ans.match(reg));
+
+const str = 'My name is R2D2';
+
+// ищем комбинацию - текст цифра текст цифра
+console.log(str.match(/\w\d\w\d/i));
+console.log(str.match(/\D/ig));
+
+
+// Обратные классы
+/*
+\D - не цифра
+\W - не буква
+\S - не пробел
+*/
+
+
+/*
+\d - класс цифры
+\w - слова, буквы
+\s - пробелы
+*/
+
+
+/* флаги
+i - независимо от регистра
+g - ищем несколько вхождений
+m - включает многострочный режим
+*/
+
+//console.log(ans.search(reg));
+
+// match - возвращаем массив
+//console.log(ans.match(reg));
+
+//const pass = prompt('Password');
+
+//console.log(pass.replace(/./g, "*"));
+//console.log(pass.replace(/\./g, "*"));
+/*
+. - берем все элементы, которые попадут в строку
+\ - экранируем символ, чтобы воспринимать как обычный текст
+\\ - экранируем слэш слэшем. аналогично прочие спецсимволы
+*/
+
+// замена дефиса на двоеточие
+//console.log('12-34-56'.replace(/-/g, ":"));
+
