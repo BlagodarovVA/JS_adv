8c79f8c6533bc13a24ff527098fbee5544c85362
Valery <11w-walerik@mail.ru>
2022-10-28 15:34:57 +0300

    107 done

--- JS106/script.js
+++ JS106/script.js
@@ -1,22 +1,18 @@
 'use strict';
 
-const data = [
-    {
-        id: 'box',
-        tag: 'div'
-    },
-    {
-        id: '',
-        tag: 'nav'
-    },
-    {
-        id: 'circle',
-        tag: 'span'
-    }
-]
+try {
+    console.log('Normal');
+    console.log(a);
+    console.log('result');
+} catch (error) {               // если в try возникает любая ошибка, перемещаемся в catch
+//catch (e) {}                  // если catch не нужен
+    console.log('Error');
+    console.log(error);         // покажет ошибку в try
+    console.log(error.name);
+    console.log(error.message);
+    console.log(error.stack);
+} finally {
 
-data.forEach(blockObj => {
-    const block = document.createElement(blockObj.tag);
-    block.setAttribute('id', blockObj.id);
-    document.body.append(block);
-})
+}
+
+console.log('Still normal');
