ff503575b4dbdcb9a0900bf8ab70c795c191ee90
Valery <11w-walerik@mail.ru>
2022-11-08 13:40:06 +0300

    114 done

---
+++ JS114 event loop/script.js
@@ -0,0 +1,31 @@
+'use strict';
+
+// console.log(1);
+
+// setTimeout(() => {
+//     console.log('timeout 2000');
+// }, 2000);
+
+// setTimeout(() => {
+//     console.log('timeout 4000');
+// }, 4000);
+
+// console.log(2);
+
+
+// let k = 0;
+// function count() {
+//     for (let i = 0; i < 1e9; i++) {
+//         k++;
+//     }
+//     alert('done');
+// }
+
+// count();
+
+
+setTimeout(() => {
+    console.log(1);
+}, 1);
+
+console.log(2);     // выполнится первым
